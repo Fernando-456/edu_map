8238c7c16ed7163691816cf259602e4bb509bf5d
Fern4nd0 456 <juan.fernanchelo456@gmail.com>
2025-12-04 04:38:35 -0600

    EDUMAP

--- EDUMAP/Program.cs
+++ EDUMAP/Program.cs
@@ -16,7 +16,7 @@ namespace EDUMAP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new ANIMACION());
             
         }
     }
